de4d5d1577570a65e65ee653c6eaf4ebb0bb5e79
Larry Kagan <larry@superiocity.com>
2016-09-20 14:33:53 -0400

    Changed view icon.

--- includes/gallery.php
+++ includes/gallery.php
@@ -2,7 +2,7 @@
 	<?php foreach ( $images as $image ) : ?>
 		<div class="thumbnail-container">
 			<a href="<?php echo $image['url'] ?>" rel="lightbox"><img src="<?php echo $image['sizes']['thumbnail'] ?>">
-				<div class="overlay"><i class="fa fa-search"></i></div>
+				<div class="overlay"><i class="fa fa-search-plus"></i></div>
 			</a>
 		</div><!-- .thumbnail-container -->
 	<?php endforeach; ?>
